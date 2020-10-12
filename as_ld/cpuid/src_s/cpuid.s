74f9d59fa21cc69e12b79b696e615d4970dc715a
Jason1735 <429824801@qq.com>
2020-10-12 23:30:43 +0800

    2020-10-12 23:27:56

---
+++ as_ld/cpuid/src_s/cpuid.s
@@ -0,0 +1,26 @@
+#cpuid.s Sample program to extract the processor Vendor ID
+.section .data
+	output: .ascii "The processor Vendor ID is 'xxxxxxxxxxxx;\n"
+	len = . - output
+
+.section .text
+.globl _start
+
+_start:
+	movl $0, %eax
+	cpuid
+	movl $output, %edi
+	movl %ebx, 28(%edi)
+	movl %edx, 32(%edi)
+	movl %ecx, 36(%edi)
+
+	movl $len, %edx
+	movl %edi, %ecx
+	movl $1, %ebx
+	movl $4, %eax
+	int $0x80
+
+	movl $0, %ebx
+	movl $1, %eax
+	int $0x80
+	
